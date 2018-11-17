615816e3881f4e3b1f05a7dfb8f0ad9c0437de4b
cutebaby0630 <lovemath0630@gmail.com>
2018-11-17 20:55:12 +0800

    E

--- src/B.java
+++ src/B.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class B {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("輸入計算值a:");
-		Scanner numa = new Scanner(System.in);
-		int a = numa.nextInt();
+		System.out.print("a = ");
+		int a = scanner.nextInt();
 		System.out.println("輸入計算值b:");
-		Scanner numb = new Scanner(System.in);
-		int b = numb.nextInt();
+		System.out.print("b = ");
+		int b = scanner.nextInt();
 		int ans = 1;
 		while (a % b != 0) {
 			ans = b;
---
+++ src/E.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class E {
+//	要麼 m 遞減，要麼 m 保持不變而 n 遞減。每次 n 達到零，m 遞減，所以 m 最終可以達到零。在 m 遞減的時候， n 的增加沒有上界
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("輸入計算值a:");
+		System.out.print("a = ");
+		int a = scanner.nextInt();
+		System.out.println("輸入計算值b:");
+		System.out.print("b = ");
+		int b = scanner.nextInt();
+		System.out.println("計算值為:"+num(a, b));
+	}
+
+	private static int num(int a, int b) {
+		while(a!=0) {
+			if(a > 0 && b == 0) {
+				return num(a-1, 1);
+			}else {
+				return num(a-1, num(a, b-1));
+			}
+		}
+			return b+1;
+		}
+	}
+
