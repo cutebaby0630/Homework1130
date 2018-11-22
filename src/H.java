268cf00e4ef9a8236a1a0b598f53e3bfca7f0a85
cutebaby0630 <lovemath0630@gmail.com>
2018-11-22 20:55:55 +0800

    H 物件導向

--- src/H.java
+++ src/H.java
@@ -1,21 +1,24 @@
 import java.util.Random;
 
 public class H {
-	public static void main(String[] args) {
-		int[] list = new int[10];
-		int min = 0;
-		int max = 0;
+	public static int[] list = new int[10];
+	public static int min = 0;
+	public static int max = 0;
+	public H() {
 		Random random = new Random();
 		for (int b = 0; b < 10; b++) {
 			int a = random.nextInt((100) + 1);
 			list[b] = a;
 		}
+	}
+	public void print() {
 		System.out.print("樣本數列: { ");
 		for (int b=0;b<list.length;b++) {
 			System.out.print(list[b] + " ");
 		}
 		System.out.println("}");
-		
+	}
+	public void comparison() {
 		for (int i = 0; i < list.length; i++) {
 			min = i;
 			for (int j = i; j < list.length; j++) {
@@ -27,6 +30,11 @@ public class H {
 			list[min] = list[i];
 			list[i] = temp;
 		}
+	}
+	public static void main(String[] args) {
+		H num =new H();
+		num.print();
+		num.comparison();
 		System.out.println("最大數值 : " + list[max]);
 		System.out.println("最小數值 : " + list[min]);
 
